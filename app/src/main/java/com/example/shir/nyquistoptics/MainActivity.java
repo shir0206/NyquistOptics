9362f7982b29f1cd72a30187594679968e4d3240
shir0206 <40990488+shir0206@users.noreply.github.com>
2018-08-30 15:35:13 +0300

    Version 1.0
    Switch to beta version of testing 1.0

--- app/src/main/java/com/example/shir/nyquistoptics/MainActivity.java
+++ app/src/main/java/com/example/shir/nyquistoptics/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.shir.nyquistoptics;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,69 +13,60 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class MainActivity extends AppCompatActivity implements Serializable {
 
-  //  TextView tv_comment; // initialize comment
+    View v_rectangleBackgroundUp; // Initialize background for activity design
+    View v_rectangleLineUp; // Initialize lines for activity design
 
-    TextView tv_txtSensorPitch, tv_txtFocalLength, tv_txtSensorSize, tv_txtSensorSizeX; //initialize inputs titles
-    EditText et_sensorPitch, et_focalLength, et_sensorSizeW, et_sensorSizeH; //initialize inputs values
+    TableLayout t_tableInput; // Initialize input table
 
-    Button btn_calculate; // initialize calculate button
+    ImageView iv_sensorPitch, iv_focalLength, iv_sensorSize; // Initialize inputs icons
+    TextView tv_txtSensorPitch, tv_txtFocalLength, tv_txtSensorSize, tv_txtSensorSizeX; // Initialize inputs titles
+    EditText et_sensorPitch, et_focalLength, et_sensorSizeW, et_sensorSizeH; // Initialize inputs values
 
-    TextView tv_txtFov, tv_txtIfov, tv_txtHfov, tv_txtVfov; //initialize outputs titles
-    TextView tv_resIfov, tv_resHfov, tv_resVfov; //initialize outputs results
+    Button btn_calculate; //  Initialize calculate button
 
-    TextView tv_txtTargetDri;
-    TextView tv_txtNatoTarget; //initialize title
-//    TextView tv_txtNatoDet, tv_txtNatoRec, tv_txtNatoIdent; //initialize outputs titles
-    TextView tv_resNatoDet, tv_resNatoRec, tv_resNatoIdent; //initialize outputs results
+    View v_rectangleLineFov, v_rectangleLineDri; // Initialize lines for activity design
 
-    TextView tv_txtHumanTarget; //initialize title
- //   TextView tv_txtHumanDet, tv_txtHumanRec, tv_txtHumanIdent; //initialize outputs titles
-    TextView tv_resHumanDet, tv_resHumanRec, tv_resHumanIdent; //initialize outputs results
+    TableLayout t_tableFov, t_tableTargetDri; // Initialize 2 output tables
 
-    TextView tv_txtObjTarget; //initialize title
- //   TextView tv_txtObjDet, tv_txtObjRec; //initialize outputs titles
-    TextView tv_resObjDet, tv_resObjRec; //initialize outputs results
+    TextView tv_txtFov; // Initialize table title
+    TextView tv_txtIfov, tv_txtHfov, tv_txtVfov; // Initialize outputs titles
+    TextView tv_resIfov, tv_resHfov, tv_resVfov; // Initialize outputs results
 
-    TextView tv_natoSize, tv_humanSize, tv_objSize;
+    TextView tv_txtTargetDri; // Initialize table title
+    TextView tv_txtNatoTarget, tv_txtHumanTarget, tv_txtObjTarget; // Initialize target titles
+    TextView tv_natoSize, tv_humanSize, tv_objSize; // Initialize target sizes titles
+    TextView tv_txtDet, tv_txtRec, tv_txtIdent; // Initialize target DRI titles
+    TextView tv_resNatoDet, tv_resNatoRec, tv_resNatoIdent; // Initialize outputs results
+    TextView tv_resHumanDet, tv_resHumanRec, tv_resHumanIdent; // Initialize outputs results
+    TextView tv_resObjDet, tv_resObjRec; // Initialize outputs results
 
-    TextView tv_txtDet, tv_txtRec,tv_txtIdent;
+    ArrayList<View> lineOutput = new ArrayList<>(); // Initialize output array to hide all lines for design before bottom click
+    ArrayList<TableLayout> tableOutput = new ArrayList<>(); // Initialize output array to hide all tables output before bottom click
+    ArrayList<TextView> textViewOutput = new ArrayList<>(); // Initialize output array to hide all TextViews output before bottom click
 
-    ImageView iv_sensorPitch, iv_focalLength, iv_sensorSize;
+    DecimalFormat formatOneDig = new DecimalFormat("0.0"); // Initialize decimal format for outputs
+    DecimalFormat formatSixDig = new DecimalFormat("0.000000"); // Initialize decimal format for outputs
 
- //   ImageView iv_sensorPitchBackground, iv_focalLengthBackground, iv_sensorSizeBackground;
+    SharedPreferences targetSizeDefaultSettings; // Save all target sizes default settings
+    SharedPreferences linePairDefaultSettings; // Save all line pairs default settings
 
-
-   // Table t_tableInput, t_tableFov,     t_tableTargetDri; ///////////////
-    View v_rectangleBackgroundUp, v_rectangleLineUp, v_rectangleLineFov,  v_rectangleLineDri; /////////
-
-
-    ArrayList<TextView> output = new ArrayList<>();
-
-    DecimalFormat formatOneDig = new DecimalFormat("0.0");
-    DecimalFormat formatSixDig = new DecimalFormat("0.000000");
-
-    SharedPreferences targetSizeDefaultSettings;
-    SharedPreferences linePairDefaultSettings;
-
-    TargetSizeHolder targetSizeHolder;
+    TargetSizeHolder targetSizeHolder; // Initialize target size Singleton
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_t2);
+        setContentView(R.layout.activity_main);
 
         hideKeyboardOnStartUp(); //Hide keyboard on start up app
         isEmptyDefaultSettings(); //Create SharedPreferences files if haven't created yet, and set default settings to the files
@@ -175,8 +165,11 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                     tv_humanSize.setText(humanSize);
                     tv_objSize.setText(objSize);
 
-                    //Turn the output TextView to visible
-                    visibleOutputTextView();
+                    //Turn the output  visible
+                    turnVisible();
+
+                    Toast.makeText(MainActivity.this, "Calculated successfully", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
@@ -188,13 +181,19 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
+
     private void setupUI() { //Initialize all TextViews, EditViews & Button
 
+        v_rectangleBackgroundUp = findViewById(R.id.v_rectangleBackgroundUp);
+        v_rectangleLineUp = findViewById(R.id.v_rectangleLineUp);
 
+        t_tableInput = findViewById(R.id.t_tableInput);
 
-       // tv_comment = findViewById(R.id.tv_comment);
-
+        iv_sensorPitch = findViewById(R.id.iv_sensorPitch);
+        iv_focalLength = findViewById(R.id.iv_focalLength);
+        iv_sensorSize = findViewById(R.id.iv_sensorSize);
         tv_txtSensorPitch = findViewById(R.id.tv_txtSensorPitch);
+
         tv_txtFocalLength = findViewById(R.id.tv_txtFocalLength);
         tv_txtSensorSize = findViewById(R.id.tv_txtSensorSize);
         tv_txtSensorSizeX = findViewById(R.id.tv_txtSensorSizeX);
@@ -204,20 +203,16 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         et_sensorSizeW = findViewById(R.id.et_sensorSizeW);
         et_sensorSizeH = findViewById(R.id.et_sensorSizeH);
 
-        iv_sensorPitch = findViewById(R.id.iv_sensorPitch);
-        iv_focalLength = findViewById(R.id.iv_focalLength);
-        iv_sensorSize = findViewById(R.id.iv_sensorSize);
-
-     //   iv_sensorPitchBackground = findViewById(R.id.iv_sensorPitchBackground);
-     //   iv_focalLengthBackground = findViewById(R.id.iv_focalLengthBackground);
-      //  iv_sensorSizeBackground = findViewById(R.id.iv_sensorSizeBackground);
-
-        v_rectangleBackgroundUp = findViewById(R.id.v_rectangleBackgroundUp);
-        v_rectangleLineUp = findViewById(R.id.v_rectangleLineUp);
-
         btn_calculate = findViewById(R.id.btn_calculate);
 
-        //From here and out TextViews are added to the output ArrayList (should be invisible)
+        // From here and out all the object are added to arrays in order to set invisible
+
+        v_rectangleLineFov = findViewById(R.id.v_rectangleLineFov);
+        v_rectangleLineDri = findViewById(R.id.v_rectangleLineDri);
+
+        t_tableFov = findViewById(R.id.t_tableFov);
+        t_tableTargetDri = findViewById(R.id.t_tableTargetDri);
+
         tv_txtFov = findViewById(R.id.tv_txtFov);
         tv_txtIfov = findViewById(R.id.tv_txtIfov);
         tv_txtHfov = findViewById(R.id.tv_txtHfov);
@@ -227,85 +222,160 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         tv_resVfov = findViewById(R.id.tv_resVfov);
 
         tv_txtTargetDri = findViewById(R.id.tv_txtTargetDri);
-        tv_txtDet = findViewById(R.id.tv_txtDet);
-        tv_txtRec = findViewById(R.id.tv_txtRec);
-        tv_txtIdent = findViewById(R.id.tv_txtIdent);
-
         tv_txtNatoTarget = findViewById(R.id.tv_txtNatoTarget);
-        tv_resNatoDet = findViewById(R.id.tv_resNatoDet);
-        tv_resNatoRec = findViewById(R.id.tv_resNatoRec);
-        tv_resNatoIdent = findViewById(R.id.tv_resNatoIdent);
-
         tv_txtHumanTarget = findViewById(R.id.tv_txtHumanTarget);
-        tv_resHumanDet = findViewById(R.id.tv_resHumanDet);
-        tv_resHumanRec = findViewById(R.id.tv_resHumanRec);
-        tv_resHumanIdent = findViewById(R.id.tv_resHumanIdent);
-
         tv_txtObjTarget = findViewById(R.id.tv_txtObjTarget);
-        tv_resObjDet = findViewById(R.id.tv_resObjDet);
-        tv_resObjRec = findViewById(R.id.tv_resObjRec);
-
         tv_natoSize = findViewById(R.id.tv_natoSize);
         tv_humanSize = findViewById(R.id.tv_humanSize);
         tv_objSize = findViewById(R.id.tv_objSize);
+        tv_txtDet = findViewById(R.id.tv_txtDet);
+        tv_txtRec = findViewById(R.id.tv_txtRec);
+        tv_txtIdent = findViewById(R.id.tv_txtIdent);
+        tv_resNatoDet = findViewById(R.id.tv_resNatoDet);
+        tv_resNatoRec = findViewById(R.id.tv_resNatoRec);
+        tv_resNatoIdent = findViewById(R.id.tv_resNatoIdent);
+        tv_resHumanDet = findViewById(R.id.tv_resHumanDet);
+        tv_resHumanRec = findViewById(R.id.tv_resHumanRec);
+        tv_resHumanIdent = findViewById(R.id.tv_resHumanIdent);
+        tv_resObjDet = findViewById(R.id.tv_resObjDet);
+        tv_resObjRec = findViewById(R.id.tv_resObjRec);
 
-        v_rectangleLineFov = findViewById(R.id.v_rectangleLineFov);
-        v_rectangleLineDri = findViewById(R.id.v_rectangleLineDri);
-
-        createOutputArrayList(); //Add the output TextViews to the ArrayList
-        invisibleOutputTextView(); //Turn the output TextViews in the ArrayList to invisible
+        createArrays();
+        turnInvisible();
 
     }
+
+
+    private void createArrays() {
+
+        createLineOutputArrayList();
+        createTableOutputArrayList();
+        createOutputArrayList();
+
+
+    }
+
+
+    private void turnInvisible() {
+
+        invisibleLineOutput();
+        invisibleTableOutput();
+        invisibleTextViewOutput();
+    }
+
+
+
+    private void turnVisible() {
+
+        visibleLineOutput();
+        visibleTableOutput();
+        visibleTextViewOutput();
+
+
+    }
+
+
+
+    private void createLineOutputArrayList () {
+        lineOutput.add(v_rectangleLineFov);
+        lineOutput.add(v_rectangleLineDri);
+    }
+
+
+
+    private void invisibleLineOutput() { //Turn the output TextViews in the ArrayList to invisible
+
+        for (View vw : lineOutput) {
+            vw.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private void visibleLineOutput() { //Turn the output TextViews in the ArrayList to visible
+
+        for (View vw : lineOutput) {
+            vw.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void createTableOutputArrayList () {
+
+        tableOutput.add(t_tableFov);
+        tableOutput.add(t_tableTargetDri);
+
+    }
+
+
+
+    private void invisibleTableOutput() { //Turn the output TextViews in the ArrayList to invisible
+
+        for (TableLayout tl : tableOutput) {
+            tl.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    private void visibleTableOutput() { //Turn the output TextViews in the ArrayList to visible
+
+        for (TableLayout tl : tableOutput) {
+            tl.setVisibility(View.VISIBLE);
+        }
+
+    }
+
 
     private void createOutputArrayList() { //Add the output TextViews to the ArrayList
 
-        output.add(tv_txtFov);
-        output.add(tv_txtIfov);
-        output.add(tv_txtHfov);
-        output.add(tv_txtVfov);
-        output.add(tv_resIfov);
-        output.add(tv_resHfov);
-        output.add(tv_resVfov);
+        textViewOutput.add(tv_txtFov);
 
-        output.add(tv_txtTargetDri);
-        output.add(tv_txtDet);
-        output.add(tv_txtRec);
-        output.add(tv_txtIdent);
+        textViewOutput.add(tv_txtIfov);
+        textViewOutput.add(tv_txtHfov);
+        textViewOutput.add(tv_txtVfov);
 
-        output.add(tv_txtNatoTarget);
-        output.add(tv_resNatoDet);
-        output.add(tv_resNatoRec);
-        output.add(tv_resNatoIdent);
+        textViewOutput.add(tv_resIfov);
+        textViewOutput.add(tv_resHfov);
+        textViewOutput.add(tv_resVfov);
 
-        output.add(tv_txtHumanTarget);
-        output.add(tv_resHumanDet);
-        output.add(tv_resHumanRec);
-        output.add(tv_resHumanIdent);
+        textViewOutput.add(tv_txtTargetDri);
 
-        output.add(tv_txtObjTarget);
-        output.add(tv_resObjDet);
-        output.add(tv_resObjRec);
+        textViewOutput.add(tv_txtNatoTarget);
+        textViewOutput.add(tv_txtHumanTarget);
+        textViewOutput.add(tv_txtObjTarget);
 
-        output.add(tv_natoSize);
-        output.add(tv_humanSize);
-        output.add(tv_objSize);
+        textViewOutput.add(tv_natoSize);
+        textViewOutput.add(tv_humanSize);
+        textViewOutput.add(tv_objSize);
 
+        textViewOutput.add(tv_txtDet);
+        textViewOutput.add(tv_txtRec);
+        textViewOutput.add(tv_txtIdent);
 
+        textViewOutput.add(tv_resNatoDet);
+        textViewOutput.add(tv_resNatoRec);
+        textViewOutput.add(tv_resNatoIdent);
+
+        textViewOutput.add(tv_resHumanDet);
+        textViewOutput.add(tv_resHumanRec);
+        textViewOutput.add(tv_resHumanIdent);
+
+        textViewOutput.add(tv_resObjDet);
+        textViewOutput.add(tv_resObjRec);
 
 
     }
 
-    private void invisibleOutputTextView() { //Turn the output TextViews in the ArrayList to invisible
+    private void invisibleTextViewOutput() { //Turn the output TextViews in the ArrayList to invisible
 
-        for (TextView tv : output) {
+        for (TextView tv : textViewOutput) {
             tv.setVisibility(View.INVISIBLE);
         }
 
     }
 
-    private void visibleOutputTextView() { //Turn the output TextViews in the ArrayList to visible
+    private void visibleTextViewOutput() { //Turn the output TextViews in the ArrayList to visible
 
-        for (TextView tv : output) {
+        for (TextView tv : textViewOutput) {
             tv.setVisibility(View.VISIBLE);
         }
 
@@ -337,7 +407,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     private void isEmptyLinePairDefaultSettings() {
         linePairDefaultSettings = getSharedPreferences("linePairDefaultSettings", Context.MODE_PRIVATE); //Create a file named "defaultSettings", private
 
-        //Check if SharedPreferences file is empty (linePairDefaultSettings file).
+        // Check if SharedPreferences file is empty (linePairDefaultSettings file).
         // If it's empty (first app usage), put default settings.
         if (linePairDefaultSettings.getString("defaultSettings_lpDet", "").isEmpty()) {
 
--- app/src/main/java/com/example/shir/nyquistoptics/SettingsActivity.java
+++ app/src/main/java/com/example/shir/nyquistoptics/SettingsActivity.java
@@ -15,22 +15,17 @@ import android.widget.Toast;
 public class SettingsActivity extends AppCompatActivity {
 
 
-    TextView tv_settingsComment; // initialize comment
-
-    TextView tv_lp, tv_lpDet, tv_lpRec, tv_lpIdent, tv_lpDetObj; //initialize inputs titles
-    EditText et_lpDet, et_lpRec, et_lpIdent, et_lpDetObj; //initialize inputs values
-
-
-    EditText et_natoTargetH, et_natoTargetW, et_humanTargetH, et_humanTargetW, et_objTargetH, et_objTargetW;
-    TextView tv_humanTargetSizeX, tv_natoTargetSizeX, tv_objTargetSizeX, tv_targetSize, tv_txtHumanTarget, tv_txtNatoTarget, tv_txtObjTarget;
-
-
-    Button btn_save;
-    //   Button btn_setDefault; // initialize buttons
-
-    //   SharedPreferences targetSizeDefaultSettings; //Copy from MainActivity
-    //   SharedPreferences linePairDefaultSettings;//Copy from MainActivity
-    //   TargetSizeHolder targetSizeHolder;//Copy from MainActivity
+    Button  btn_refresh, btn_save;
+    View  v_rectangleBackgroundDown, v_rectangleBackgroundUp, v_rectangleLineDown, v_rectangleLineLp;
+    TextView  tv_lp;
+    TextView  tv_lpDet, tv_lpDetObj, tv_lpIdent, tv_lpRec;
+    EditText  et_lpDet, et_lpDetObj, et_lpIdent, et_lpRec;
+    TextView  tv_targetSize;
+    TextView  tv_txtHumanTargetSize, tv_txtNatoTargetSize, tv_txtObjTargetSize;
+    TextView  tv_natoTargetSizeX, tv_humanTargetSizeX, tv_objTargetSizeX;
+    EditText  et_natoTargetW, et_natoTargetH;
+    EditText  et_humanTargetW, et_humanTargetH;
+    EditText  et_objTargetW, et_objTargetH;
 
 
     @Override
@@ -40,20 +35,6 @@ public class SettingsActivity extends AppCompatActivity {
         setupUI();
         displaySettings();
         hideKeyboardOnStartUp();
-
-        /*
-        btn_setDefault.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                isEmptyDefaultSettings(); //Create SharedPreferences files if haven't created yet, and set default settings to the files
-                SetDefaultSettings(); //Set default settings derived from SharedPreferences files to the objects
-
-                Toast.makeText(SettingsActivity.this, "Default settings set", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-    */
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +49,6 @@ public class SettingsActivity extends AppCompatActivity {
 
                 //If all fields are filled, continue
                 else {
-
 
                     saveSettings(view);
                     SetSettingsToClass(view);
@@ -89,39 +69,46 @@ public class SettingsActivity extends AppCompatActivity {
 
     private void setupUI() {
 
-        tv_settingsComment = findViewById(R.id.tv_settingsComment);
-        tv_lp = findViewById(R.id.tv_lp);
-        tv_lpDet = findViewById(R.id.tv_lpDet);
-        tv_lpRec = findViewById(R.id.tv_lpRec);
-        tv_lpIdent = findViewById(R.id.tv_lpIdent);
-        tv_lpDetObj = findViewById(R.id.tv_lpDetObj);
+        btn_refresh = findViewById(R.id.btn_refresh);
+        btn_save = findViewById(R.id.btn_save);
 
+        v_rectangleBackgroundDown = findViewById(R.id.v_rectangleBackgroundDown);
+        v_rectangleBackgroundUp = findViewById(R.id.v_rectangleBackgroundUp);
+        v_rectangleLineDown = findViewById(R.id.v_rectangleLineDown);
+        v_rectangleLineLp = findViewById(R.id.v_rectangleLineLp);
+
+
+        tv_lp = findViewById(R.id.tv_lp);
+
+        tv_lpDet = findViewById(R.id.tv_lpDet);
+        tv_lpDetObj = findViewById(R.id.tv_lpDetObj);
+        tv_lpIdent = findViewById(R.id.tv_lpIdent);
+        tv_lpRec = findViewById(R.id.tv_lpRec);
 
         et_lpDet = findViewById(R.id.et_lpDet);
-        et_lpRec = findViewById(R.id.et_lpRec);
-        et_lpIdent = findViewById(R.id.et_lpIdent);
         et_lpDetObj = findViewById(R.id.et_lpDetObj);
+        et_lpIdent = findViewById(R.id.et_lpIdent);
+        et_lpRec = findViewById(R.id.et_lpRec);
 
-
-        et_humanTargetH = findViewById(R.id.et_humanTargetH);
-        et_humanTargetW = findViewById(R.id.et_humanTargetW);
-        et_natoTargetH = findViewById(R.id.et_natoTargetH);
-        et_natoTargetW = findViewById(R.id.et_natoTargetW);
-        et_objTargetH = findViewById(R.id.et_objTargetH);
-        et_objTargetW = findViewById(R.id.et_objTargetW);
-        tv_humanTargetSizeX = findViewById(R.id.tv_humanTargetSizeX);
-        tv_natoTargetSizeX = findViewById(R.id.tv_natoTargetSizeX);
-        tv_objTargetSizeX = findViewById(R.id.tv_objTargetSizeX);
         tv_targetSize = findViewById(R.id.tv_targetSize);
-        tv_txtHumanTarget = findViewById(R.id.tv_txtHumanTarget);
-        tv_txtNatoTarget = findViewById(R.id.tv_txtNatoTarget);
-        tv_txtObjTarget = findViewById(R.id.tv_txtObjTarget);
 
+        tv_txtHumanTargetSize = findViewById(R.id.tv_txtHumanTargetSize);
+        tv_txtNatoTargetSize = findViewById(R.id.tv_txtNatoTargetSize);
+        tv_txtObjTargetSize = findViewById(R.id.tv_txtObjTargetSize);
 
-        btn_save = findViewById(R.id.btn_save);
-        // double lpDet = LinePair.getLpDet();
-        //  String text = Double.toString(lpDet);
-        // et_lpDet.setText(text);
+        tv_natoTargetSizeX = findViewById(R.id.tv_natoTargetSizeX);
+        tv_humanTargetSizeX = findViewById(R.id.tv_humanTargetSizeX);
+        tv_objTargetSizeX = findViewById(R.id.tv_objTargetSizeX);
+
+        et_natoTargetW = findViewById(R.id.et_natoTargetW);
+        et_natoTargetH = findViewById(R.id.et_natoTargetH);
+
+        et_humanTargetW = findViewById(R.id.et_humanTargetW);
+        et_humanTargetH = findViewById(R.id.et_humanTargetH);
+
+        et_objTargetW = findViewById(R.id.et_objTargetW);
+        et_objTargetH = findViewById(R.id.et_objTargetH);
+
     }
 
     public void saveSettings(View view) {
@@ -261,111 +248,4 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
-    /*
-    private void isEmptyDefaultSettings() { //Create SharedPreferences files if haven't created yet, and set default settings to the files
-        isEmptyLinePairDefaultSettings();
-        isEmptyTargetSizeDefaultSettings();
-    }//Copy from MainActivity
-
-    private void isEmptyLinePairDefaultSettings() {
-        linePairDefaultSettings = getSharedPreferences("linePairDefaultSettings", Context.MODE_PRIVATE); //Create a file named "defaultSettings", private
-
-        //Check if SharedPreferences file is empty (linePairDefaultSettings file).
-        // If it's empty (first app usage), put default settings.
-        if (linePairDefaultSettings.getString("defaultSettings_lpDet", "").isEmpty()) {
-
-            SharedPreferences.Editor editor = linePairDefaultSettings.edit(); //Create an editor to edit the file
-
-            //Put default settings
-            editor.putString("defaultSettings_lpDet", "2");
-            editor.putString("defaultSettings_lpRec", "6");
-            editor.putString("defaultSettings_lpIdent", "10");
-            editor.putString("defaultSettings_lpDetObj", "1.2");
-
-            editor.apply(); //Saves the changes
-
-        }
-    }//Copy from MainActivity
-
-    private void isEmptyTargetSizeDefaultSettings() {
-        targetSizeDefaultSettings = getSharedPreferences("targetSizeDefaultSettings", Context.MODE_PRIVATE); //Create a file named "defaultSettings", private
-
-        //Check if SharedPreferences file is empty (targetSizeDefaultSettings file).
-        // If it's empty (first app usage), put default settings.
-        if (targetSizeDefaultSettings.getString("defaultSettings_natoTargetW", "").isEmpty()) {
-
-            SharedPreferences.Editor editor = targetSizeDefaultSettings.edit(); //Create an editor to edit the file
-
-            //Put default settings
-            editor.putString("defaultSettings_natoTargetW", "2.3");
-            editor.putString("defaultSettings_natoTargetH", "2.3");
-
-            editor.putString("defaultSettings_humanTargetW", "0.5");
-            editor.putString("defaultSettings_humanTargetH", "1.7");
-
-            editor.putString("defaultSettings_objTargetW", "0.5");
-            editor.putString("defaultSettings_objTargetH", "0.5");
-
-            editor.apply(); //Saves the changes
-
-        }
-    }//Copy from MainActivity
-
-    private void SetDefaultSettings() { //Set default settings derived from SharedPreferences files to the objects
-        SetLinePairDefaultSettingsToClass();
-        SetTargetSizeDefaultSettingsToObjects();
-    }//Copy from MainActivity
-
-    private void SetLinePairDefaultSettingsToClass() { //Print the saved settings
-        linePairDefaultSettings = getSharedPreferences("linePairDefaultSettings", Context.MODE_PRIVATE);
-
-        //Convert the user input from String to double
-        double lpDet = Double.parseDouble(linePairDefaultSettings.getString("defaultSettings_lpDet", ""));
-        double lpRec = Double.parseDouble(linePairDefaultSettings.getString("defaultSettings_lpRec", ""));
-        double lpIdent = Double.parseDouble(linePairDefaultSettings.getString("defaultSettings_lpIdent", ""));
-        double lpDetObj = Double.parseDouble(linePairDefaultSettings.getString("defaultSettings_lpDetObj", ""));
-
-        //Set the parameters in LinePair classes according to the user input
-        LinePair.setLpDet(lpDet);
-        LinePair.setLpRec(lpRec);
-        LinePair.setLpIdent(lpIdent);
-        LinePair.setLpDetObj(lpDetObj);
-
-    }//Copy from MainActivity
-
-    private void SetTargetSizeDefaultSettingsToObjects() {
-        //get the values from targetSizeDefaultSettings Shared Preferences and convert from String to double
-        double natoTargetW = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_natoTargetW", ""));
-        double natoTargetH = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_natoTargetH", ""));
-
-        double humanTargetW = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_humanTargetW", ""));
-        double humanTargetH = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_humanTargetH", ""));
-
-        double objTargetW = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_objTargetW", ""));
-        double objTargetH = Double.parseDouble(targetSizeDefaultSettings.getString("defaultSettings_objTargetH", ""));
-
-
-        //Initialize TargetSize class instances (using singleton - TargetSizeHolder class)
-
-        targetSizeHolder = TargetSizeHolder.getInstance();
-
-        //Check if a specific TargetSize instance exists in TargetSizeHolder class (in hashmap targetSizeCollection).
-        //If not, add the instance.
-        //Every instance initialized with his own unique W*H, as described in SharedPreferences (targetSizeDefaultSettings file)
-
-        if (!targetSizeHolder.hasTargetSize("natoTargetSize")) {
-            targetSizeHolder.addTargetSize("natoTargetSize", new TargetSize(natoTargetW, natoTargetH));
-        }
-
-        if (!targetSizeHolder.hasTargetSize("humanTargetSize")) {
-            targetSizeHolder.addTargetSize("humanTargetSize", new TargetSize(humanTargetW, humanTargetH));
-        }
-
-        if (!targetSizeHolder.hasTargetSize("objTargetSize")) {
-            targetSizeHolder.addTargetSize("objTargetSize", new TargetSize(objTargetW, objTargetH));
-        }
-
-    }//Copy from MainActivity
-
-*/
 }
