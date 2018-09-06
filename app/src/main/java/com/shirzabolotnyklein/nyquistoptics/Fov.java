f2a3f500853fe3cf2d2ab7fc2908b09ab66e10fb
shir0206 <40990488+shir0206@users.noreply.github.com>
2018-09-06 14:01:47 +0300

    Version 1.2
    -Change package name to upload to play store

---
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/Fov.java
@@ -0,0 +1,92 @@
+package com.shirzabolotnyklein.nyquistoptics;
+
+/**
+ * This class contains all the field of view (FOV) parameters and their calculations (static class).
+ */
+public class Fov {
+
+    //------------------------------------- Parameters -------------------------------------
+
+    /**
+     * The calculated value of instantaneous field of view (IFOV).
+     */
+    private static double ifov;
+
+    /**
+     * The calculated value of horizontal field of view (HFOV).
+     */
+    private static double hfov;
+
+    /**
+     * The calculated value of vertical field of view (VFOV).
+     */
+    private static double vfov;
+
+    //------------------------------------- Formulas -------------------------------------
+
+    /**
+     * The formula that calculates the value of instantaneous field of view (IFOV).
+     * @return = The value of instantaneous field of view (IFOV).
+     */
+    private static double calcIfov() {
+        double ifov;
+        ifov = Properties.getSensorPitch() / (Properties.getFocalLength() * 1000);
+        return ifov;
+    }
+
+    /**
+     * The formula that calculates the value of horizontal field of view (HFOV).
+     * @return = The value of horizontal field of view (HFOV).
+     */
+    private static double calcHfov() {
+        double hfov;
+        hfov = 2 * Math.atan((Properties.getSensorPitch() * SensorSize.getWidth()) / (2 * 1000 * Properties.getFocalLength())) * 180 / Math.PI;
+        return hfov;
+    }
+
+    /**
+     * The formula that calculates the value of vertical field of view (VFOV).
+     * @return = The value of vertical field of view (VFOV).
+     */
+    private static double calcVfov() {
+        double vfov;
+        vfov = hfov * (SensorSize.getHeight() / SensorSize.getWidth());
+        return vfov;
+    }
+
+    //------------------------------------- Getters & Setters -------------------------------------
+
+    public static double getIfov() {
+        return ifov;
+    }
+
+    public static void setIfov() {
+        Fov.ifov = calcIfov();
+    }
+
+    public static double getHfov() {
+        return hfov;
+    }
+
+    public static void setHfov() {
+        Fov.hfov = calcHfov();
+    }
+
+    public static double getVfov() {
+        return vfov;
+    }
+
+    public static void setVfov() {
+        Fov.vfov = calcVfov();
+    }
+
+    /**
+     * Setter for the parameters: ifov, hfov, vfov.
+     */
+    public static void setFov() {
+        setIfov();
+        setHfov();
+        setVfov();
+    }
+
+}
---
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/WelcomeActivity.java
@@ -0,0 +1,53 @@
+package com.shirzabolotnyklein.nyquistoptics;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    /**
+     *  Seconds of Appearance welcome logo
+     */
+    private int SLEEP_TIMER = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
+
+        getSupportActionBar().hide();
+
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+
+    }
+
+    /**
+     * Display the logo
+     */
+    private class LogoLauncher extends Thread {
+        public void run() {
+            try {
+                sleep(1000 * SLEEP_TIMER);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            WelcomeActivity.this.finish();
+        }
+    }
+
+
+}
+
