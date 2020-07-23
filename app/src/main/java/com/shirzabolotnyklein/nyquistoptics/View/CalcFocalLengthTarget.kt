0cf6e59f652aa34a7746bdc1aacb1546fa9b8e4d
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-07-23 11:28:07 +0300

    Added new layout

---
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/CalcFocalLengthTarget.kt
@@ -0,0 +1,12 @@
+package com.shirzabolotnyklein.nyquistoptics.View
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.shirzabolotnyklein.nyquistoptics.R
+
+class CalcFocalLengthTarget : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calc_focal_length_target)
+    }
+}
