d836ca22fb650543f920b51075015b5ba0bd59e5
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-09-28 16:17:36 +0300

    Created popup message with two new activites

---
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/CalcFocalLengthFOV.java
@@ -0,0 +1,16 @@
+package com.shirzabolotnyklein.nyquistoptics.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shirzabolotnyklein.nyquistoptics.R;
+
+public class CalcFocalLengthFOV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.act_calc_focal_length_fov);
+    }
+}
