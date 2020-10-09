a34407bbe7066ccb1a9566ea80ea8956008c3800
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-10-09 17:17:18 +0300

    Fixed buggy CalcFocalLengthTarget.kt

--- app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/CalcFocalLengthTarget.kt
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/CalcFocalLengthTarget.kt
@@ -1,10 +1,11 @@
 package com.shirzabolotnyklein.nyquistoptics.View
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
 import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -24,7 +25,7 @@ class CalcFocalLengthTarget : AppCompatActivity() {
     var tv_resFocalLengthH: TextView? = null
     var tv_resFocalLengthW: TextView? = null
     var btn_calc: Button? = null
-    var vibrator: Vibrator? = null
+    private var vibrator: Vibrator? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,86 +35,100 @@ class CalcFocalLengthTarget : AppCompatActivity() {
         // Set up UI
         setupUI()
 
-        // Get the vibrator
-        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        btn_calc.setOnClickListener(View.OnClickListener { // Vibrate
-            vibrator.vibrate(50)
+        btn_calc?.setOnClickListener {
+            phoneVib()
             if (isValid()) {
                 Util.hideKeyboard(this@CalcFocalLengthTarget)
                 val calculationController = CalculationController()
 
-                val dimensionW: Double = et_dimensionW.getText().toString().toDouble()
-                val dimensionH: Double = et_dimensionH.getText().toString().toDouble()
-                val targetSizeW: Double = et_targetSizeW.getText().toString().toDouble()
-                val targetSizeH: Double = et_targetSizeH.getText().toString().toDouble()
-                val sensorPitch: Double = et_sensorPitch.getText().toString().toDouble()
-                val targetRange: Double = et_targetRange.getText().toString().toDouble()
+                val dimensionW: Double = et_dimensionW?.text.toString().toDouble()
+                val dimensionH: Double = et_dimensionH?.text.toString().toDouble()
+                val targetSizeW: Double = et_targetSizeW?.text.toString().toDouble()
+                val targetSizeH: Double = et_targetSizeH?.text.toString().toDouble()
+                val sensorPitch: Double = et_sensorPitch?.text.toString().toDouble()
+                val targetRange: Double = et_targetRange?.text.toString().toDouble()
 
 
-
-
-                val focalLengthW = java.lang.Double.toString(calculationController.calcFocalLengthWidthTarget(
+                val focalLengthW = (calculationController.calcFocalLengthWidthTarget(
                         dimensionW,
                         targetSizeW,
                         sensorPitch,
                         targetRange))
 
 
-
-                val focalLengthH = java.lang.Double.toString(calculationController.calcFocalLengthHeightTarget(dimensionW,
+                val focalLengthH = (calculationController.calcFocalLengthHeightTarget(
                         dimensionH,
                         targetSizeH,
                         sensorPitch,
                         targetRange))
 
-                tv_resFocalLengthW.setText(focalLengthW)
-                tv_resFocalLengthH.setText(focalLengthH)
+                tv_resFocalLengthW?.text = focalLengthW.toString()
+                tv_resFocalLengthH?.text = focalLengthH.toString()
             }
-        })
+        }
 
     }
 
     private fun setupUI() {
-        et_targetSizeW = findViewById<EditText>(R.id.et_targetSizeW)
-        et_targetSizeH = findViewById<EditText>(R.id.et_targetSizeH)
-        et_targetRange = findViewById<EditText>(R.id.et_targetRange)
-        et_dimensionW = findViewById<EditText>(R.id.et_dimensionW)
-        et_dimensionH = findViewById<EditText>(R.id.et_resTargetSizeH)
-        et_sensorPitch = findViewById<EditText>(R.id.et_sensorPitch)
-        tv_resFocalLengthH = findViewById<TextView>(R.id.tv_resFocalLengthH)
-        tv_resFocalLengthW = findViewById<TextView>(R.id.tv_resFocalLengthW)
-        btn_calc = findViewById<Button>(R.id.btn_calc)
+        et_targetSizeW = findViewById(R.id.et_targetSizeW)
+        et_targetSizeH = findViewById(R.id.et_targetSizeH)
+        et_targetRange = findViewById(R.id.et_targetRange)
+        et_dimensionW = findViewById(R.id.et_dimensionW)
+        et_dimensionH = findViewById(R.id.et_dimensionH)
+        et_sensorPitch = findViewById(R.id.et_sensorPitch)
+        tv_resFocalLengthH = findViewById(R.id.tv_resFocalLengthH)
+        tv_resFocalLengthW = findViewById(R.id.tv_resFocalLengthW)
+        btn_calc = findViewById(R.id.btn_calc)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    }
+
+    private fun phoneVib(){
+
+        if (vibrator!!.hasVibrator()) { // Vibrator availability checking
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator!!.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE)) // New vibrate method for API Level 26 or higher
+            } else {
+                vibrator!!.vibrate(50) // Vibrate method for below API Level 26
+            }
+        }
     }
 
     private fun isValid(): Boolean {
         val msg = " is missing."
         var value = ""
-        if (et_targetSizeW.getText().toString() == "") {
-            value = "Target Size width"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
-        } else if (et_targetSizeH.getText().toString() == "") {
-            value = "Target Size height"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
-        } else if (et_targetRange.getText().toString() == "") {
-            value = "Target Range"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
-        } else if (et_dimensionW.getText().toString() == "") {
-            value = "Dimension width"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
-        } else if (et_dimensionH.getText().toString() == "") {
-            value = "Dimension height"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
-        } else if (et_sensorPitch.getText().toString() == "") {
-            value = "Sensor Pitch"
-            Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
-            return false
+        when {
+            et_targetSizeW?.getText().toString() == "" -> {
+                value = "Target Size width"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            et_targetSizeH?.getText().toString() == "" -> {
+                value = "Target Size height"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            et_targetRange?.getText().toString() == "" -> {
+                value = "Target Range"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            et_dimensionW?.getText().toString() == "" -> {
+                value = "Dimension width"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            et_dimensionH?.getText().toString() == "" -> {
+                value = "Dimension height"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            et_sensorPitch?.getText().toString() == "" -> {
+                value = "Sensor Pitch"
+                Toast.makeText(this@CalcFocalLengthTarget, value + msg, Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else -> return true
         }
-        return true
     }
 }
