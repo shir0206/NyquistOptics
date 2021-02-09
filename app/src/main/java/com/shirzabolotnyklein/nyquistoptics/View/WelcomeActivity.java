d3ab376cb6c7f3f53793f4fad12688bd4911cfbd
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2021-02-09 21:02:12 +0200

    1) Fixed settings UI 2) Added splash screen animations

--- app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/WelcomeActivity.java
+++ app/src/main/java/com/shirzabolotnyklein/nyquistoptics/View/WelcomeActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 import com.shirzabolotnyklein.nyquistoptics.Model.ConstantsKt;
 import com.shirzabolotnyklein.nyquistoptics.R;
@@ -14,20 +18,15 @@ public class WelcomeActivity extends AppCompatActivity {
     /**
      * Seconds of Appearance welcome logo
      */
-    private int SLEEP_TIMER = 2;
+    private int SLEEP_TIMER =2;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Full screen
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.act_welcome);
-
         getSupportActionBar().hide();
-
+        imageView=findViewById(R.id.imageView);
         LogoLauncher logoLauncher = new LogoLauncher();
         logoLauncher.start();
 
@@ -40,14 +39,22 @@ public class WelcomeActivity extends AppCompatActivity {
     private class LogoLauncher extends Thread {
         public void run() {
             try {
+
+                Animation animation = new AlphaAnimation(0, 1); //to change visibility from visible to invisible
+                animation.setDuration(2000); //1 second duration for each animation cycle
+                animation.setInterpolator(new LinearInterpolator());
+                animation.setRepeatCount(Animation.INFINITE); //repeating indefinitely
+                animation.setRepeatMode(Animation.REVERSE); //animation will start from end point once ended.
+                imageView.startAnimation(animation); //to start animation
                 sleep(ConstantsKt.OneThousand * SLEEP_TIMER);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
             Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
             startActivity(intent);
-            WelcomeActivity.this.finish();
+            finish();
         }
     }
 
